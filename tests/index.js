eb1f90e8a24bd5fa65e3a3040d7a8381328c5c82
Stanislav Popov <popstas@gmail.com>
2018-07-01 22:40:15 +0500

    test: тестовый урл поменян на боевой https://whatis.dialogs.popstas.ru

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,10 @@
 'use strict'
 const { Scenarios } = require('./scenarios');
+const url = process.env.URL || 'https://whatis.dialogs.popstas.ru';
+// const url = process.env.URL || 'https://localhost:3002';
 
 (async() => {
-  const scenarios = new Scenarios('./static/scenarios.yml', 'http://localhost:3002');
+  const scenarios = new Scenarios('./static/scenarios.yml', url);
   try{
     const isErrors = await scenarios.run();
     console.log('exit');
