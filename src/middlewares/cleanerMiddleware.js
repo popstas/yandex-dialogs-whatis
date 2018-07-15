95aedc3af0d5aa98879eb63c9c9547cb2f23b4ca
Stanislav Popov <popstas@gmail.com>
2018-07-15 23:40:24 +0500

    feat: убирать "что" из запроса из Алисы "скажи второй памяти что ..."

--- src/middlewares/cleanerMiddleware.js
+++ src/middlewares/cleanerMiddleware.js
@@ -2,6 +2,11 @@
 module.exports = () => {
   return async ctx => {
     ctx.message = ctx.message.replace(/^(Алиса )?(привет )?(слушай )?(а )?(скажи )?(напомни )?/, '');
+
+    // скажи подразумевает запись, а "что" здесь естественно напрашивается
+    if(ctx.req.request.original_utterance.toLowerCase().match(/^скажи/)){
+      ctx.message = ctx.message.replace(/^что /, '');
+    }
     return ctx;
   };
 };
