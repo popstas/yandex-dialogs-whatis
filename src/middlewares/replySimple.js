c4f4ac6e332354d38868748b32eb52469280a28b
Stanislav Popov <popstas@gmail.com>
2018-07-21 00:08:08 +0500

    wip: fix

--- src/middlewares/replySimple.js
+++ src/middlewares/replySimple.js
@@ -7,7 +7,7 @@ module.exports = () => async ctx => {
 
     if (Array.isArray(buttons)) {
       for (let i in buttons) {
-        replyMessage.addButton({ ...ctx.buttonBuilder.text(buttons[i]).get() });
+        replyMessage.addButton({ ...ctx.buttonBuilder.title(buttons[i]).get() });
       }
     }
 
