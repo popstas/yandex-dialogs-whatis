2c1bfd91b4829b4fcaac0f60520f75c12bc3343c
Stanislav Popov <popstas@gmail.com>
2018-07-24 20:06:20 +0500

    fix: goodbye: показания

--- src/matchers/goodbye.js
+++ src/matchers/goodbye.js
@@ -1,4 +1,4 @@
 const strings = require('./strings');
 
 module.exports = () => ctx =>
-  ctx.message.match(/^(пока|спасибо пока|отбой|выключи|отбой|все|всё|хватит|закрой)/i);
+  ctx.message.match(/^(пока |пока$|спасибо пока|отбой$|выключи|отбой|всё|хватит|закрой)/i);
