b5d99ed3d9288c46a5d0804b5c60e4033546f952
Stanislav Popov <popstas@gmail.com>
2019-01-08 21:55:51 +0500

    fix: chatbase отключен на тестовом

--- src/middlewares/chatbase.js
+++ src/middlewares/chatbase.js
@@ -2,18 +2,20 @@
 module.exports = api_key => (ctx, next) => {
   if (!api_key) return next(ctx);
 
-  
   ctx.chatbase = require('@google/chatbase')
     .setApiKey(api_key) // Your Chatbase API Key
-    .setPlatform(ctx.data.meta.client_id) // The platform you are interacting with the user over
+    .setPlatform(ctx.data.meta.client_id); // The platform you are interacting with the user over
 
   // функцию нужно вызвать в самом конце, перед отправкой ответа пользователю
   ctx.chatbase.onShutdown = async text => {
+    if (process.env.NODE_ENV != 'production' || ctx.message == 'ping') return;
+
     // запрос
     ctx.chatbase
       .newMessage()
       .setAsTypeUser()
       .setMessage(ctx.message)
+      .setTimestamp(Date.now().toString())
       .setUserId(ctx.userId)
       .setCustomSessionId(ctx.sessionId)
       .setMessageId(ctx.messageId)
@@ -26,6 +28,7 @@ module.exports = api_key => (ctx, next) => {
       .newMessage()
       .setAsTypeAgent()
       .setMessage(text)
+      .setTimestamp(Date.now().toString())
       .setUserId(ctx.userId)
       .setCustomSessionId(ctx.sessionId)
       .setMessageId(ctx.messageId)
