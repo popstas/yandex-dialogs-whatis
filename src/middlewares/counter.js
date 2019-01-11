afafd71019bf4de8c84ccfc2eb61d8b95cf14a89
Stanislav Popov <popstas@gmail.com>
2019-01-12 01:39:50 +0500

    feat: visitor.lastVisitDate

--- src/middlewares/counter.js
+++ src/middlewares/counter.js
@@ -7,7 +7,8 @@ module.exports = () => (ctx, next) => {
   ctx.user.state.visitor = ctx.user.state.visitor || {
     visits: 0, // кол-во визитов (визит - группа сообщений в пределах 10 минут)
     lastMessageDate: 0, // время последнего сообщения навыку
-    lastMessageLong: false // давно не был в навыке
+    lastMessageLong: false, // давно не был в навыке
+    lastVisitDate: 0 // время последнего визита
   };
 
   // визит
@@ -24,6 +25,7 @@ module.exports = () => (ctx, next) => {
 
   if (isNewVisit) {
     ctx.user.state.visitor.visits++;
+    ctx.user.state.visitor.lastVisitDate = ctx.user.state.visitor.lastMessageDate;
     if (ctx.message != 'ping') {
       console.log('');
       ctx.logMessage(
