08869367e24b740f48b2effeebc5f2b214521753
Stanislav Popov <popstas@gmail.com>
2019-01-23 21:20:20 +0500

    fix: проверка ответов от Алисы в session

--- src/middlewares/logMessage.js
+++ src/middlewares/logMessage.js
@@ -2,6 +2,13 @@
 module.exports = () => (ctx, next) => {
   ctx.logMessage = message => {
     const sessId = ctx.session.id ? ctx.session.id.toString().substring(0, 4) : '';
+
+    if(ctx.user.state.visitor.visits > 10){
+      console.log(
+        `[${ctx.data.session.message_id}][${ctx.data.session.new}}]`
+      );
+    }
+
     console.log(
       `[${sessId}][${ctx.user.state.visitor.visits}][${ctx.user.state.visit.messages}] ${message}`
     );
