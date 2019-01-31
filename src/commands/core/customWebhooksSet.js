30a9fe622a3aca6499a1e868e8bcd2fa5fc7cbcc
Stanislav Popov <popstas@gmail.com>
2019-01-31 22:02:44 +0500

    fix: все хуки не влазят, добавление по одному

--- src/commands/core/customWebhooksSet.js
+++ src/commands/core/customWebhooksSet.js
@@ -4,17 +4,22 @@ const matchers = require('../../matchers');
 module.exports = {
   intent: 'customWebhooksSet',
   matcher(ctx) {
-    return ctx.message.match(/^\[/) ? 1 : 0;
+    return ctx.originalUtterance.match(/^\{/) ? 1 : 0;
   },
 
   async handler(ctx) {
-    ctx.logMessage(`> ${ctx.message} (customWebhooksSet)`);
+    ctx.logMessage(`> ${ctx.originalUtterance} (customWebhooksSet)`);
 
     try {
-      let webhooks = JSON.parse(ctx.message);
-      if (!webhooks) throw 'parse error';
+      let webhook = JSON.parse(ctx.originalUtterance);
+      if (!webhook) throw 'parse error';
+      let webhooks = ctx.user.state.webhooks || [];
 
-      webhooks = webhooks.filter(webhook => webhook.url && webhook.strings);
+      foundIndex = webhooks.findIndex(h => h.name == webhook.name);
+      if (foundIndex != -1) webhooks[foundIndex] = webhook;
+      else webhooks.push(webhook);
+
+      webhooks = webhooks.filter(webhook => webhook.name && webhook.url && webhook.strings);
       if (webhooks.length == 0) throw 'empty list';
 
       ctx.user.state.webhooks = webhooks;
