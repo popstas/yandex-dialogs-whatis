c72e8a622949d09235367a92b154cf462e5abd59
Stanislav Popov <popstas@gmail.com>
2019-02-02 18:19:33 +0500

    feat: начальная поддержка переменных в запросах к вебхукам

--- src/commands/core/customWebhooks.js
+++ src/commands/core/customWebhooks.js
@@ -2,19 +2,32 @@
 const axios = require('axios');
 const matchers = require('../../matchers');
 
+const placeholderRegex = /\{\{[a-zA-Z0-9_-]+\}\}/;
+const values = {};
+
+// умеет отрабатывать строки типа "скачай фильм {{q}}"
 const getWebhook = ctx => {
-  return ctx.user.state.webhooks.find(webhook => {
+  let matched = false;
+  let found = ctx.user.state.webhooks.find(webhook => {
     webhook.regs = webhook.strings.map(string => {
+      string = string.replace(placeholderRegex, '(.*)?');
       const reg = new RegExp(string);
       return reg;
     });
 
-    const match = webhook.regs.find(reg => {
+    const matchReg = webhook.regs.find(reg => {
       return reg.test(ctx.message);
     });
 
-    return match;
+    if (matchReg) matched = matchReg.exec(ctx.message);
+    return matchReg;
   });
+
+  if (found) {
+    found.matched = matched;
+  }
+
+  return found;
 };
 
 module.exports = {
@@ -25,13 +38,18 @@ module.exports = {
         strings: ['тестовый вебхук'],
         url: 'https://yandex.ru'
       } */
-    return getWebhook(ctx) ? 1 : 0;
+    return getWebhook(ctx) ? 0.9 : 0;
   },
 
   async handler(ctx) {
     ctx.logMessage(`> ${ctx.message} (customWebhooks)`);
     const webhook = getWebhook(ctx);
-    const answer = axios.get(webhook.url);
+
+    // if (webhook.url.match(placeholderRegex)) {
+    values.q = webhook.matched[1]; // TODO: всегда передается q, хотя могут быть другие имена
+    // webhook.url = webhook.url.replace(placeholderRegex, );
+    // }
+    const answer = axios.get(webhook.url, { params: values });
     return ctx.reply(webhook.answer || 'Готово');
   }
 };
